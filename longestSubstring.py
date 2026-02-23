d41cec847d9e06bd2794dbc5d6d1452819f91af1
munamadan <dipankharel@gmail.com>
2026-02-23 11:39:05 +0545

    Length of Longest Substring Withour Characters Repeating

---
+++ longestSubstring.py
@@ -0,0 +1,31 @@
+#brute force method
+class Solution:
+    def lengthOfLongestSubstring(self, s:str) -> int:
+        if len(s)==0:
+            return 0
+        l,r = 0,0
+        chars = set()
+        maxLen = 0
+        while r<len(s):
+            while s[r] in chars:
+                chars.remove(s[l])
+                l+=1
+            maxLen = max(r-l+1, maxLen)
+            chars.add(s[r])
+            r+=1
+        return maxLen
+
+#neetcode method(not much difference, he just didn't have the checker for 0 length and maxLen is set at 0)
+class Solution:
+    def lengthOfLongestSubstring(self, s:str) -> int:
+        l = 0
+        charSet = set()
+        res = 0
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l+=1
+            charSet.add(s[r])
+            res = max(res, r-l+1)
+            r+=1
+        return res
