78fae2308e823b71ed3952a9422205390feae180
munamadan <dipankharel@gmail.com>
2026-02-28 23:11:29 +0545

    Remove Duplicated

---
+++ removeDuplicated.py
@@ -0,0 +1,7 @@
+def removeDuplicates(self, nums: list[int]) -> int:
+    l = 0
+    for r in range(1, len(nums)):
+        if nums[r]!=nums[l]:
+            l+=1
+            nums[l] = nums[r]
+    return l+1
