29a0513a7f354eb728caa0f443b488b95136a2de
munamadan <dipankharel@gmail.com>
2026-03-15 20:36:43 +0545

    Rotate array rightwards

---
+++ rotateArray.py
@@ -0,0 +1,10 @@
+#brute force method (timeout hunchha)
+class Solution:
+    def rotate(self, nums:list[int], k:int) -> None:
+        while k!=0:
+            last = nums[-1]
+            for i in range(len(nums)-1, -1, -1):
+                if k==0: return nums
+                nums[i] = nums[i-1]
+            nums[0] = last
+            k-=1
