42d6829b16d427638a64c22ab24dd89a7bb3906f
munamadan <dipankharel@gmail.com>
2026-03-22 11:33:07 +0545

    Plus One

---
+++ plusOne.py
@@ -0,0 +1,16 @@
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        carry = 1
+        for i in range(len(digits) - 1, -1, -1):
+            digits[i] += carry
+            if digits[i] == 10:
+                digits[i] = 0
+            else:
+                carry = 0
+                break
+        if carry == 1:
+            digits.insert(0, 1)
+        return digits
+
+
+# much harder than it looks!!!!
