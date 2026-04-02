641b1c02b5913901a932753a1095405f96935806
munamadan <dipankharel@gmail.com>
2026-04-02 11:08:07 +0545

    Seach in 2D Matrix

---
+++ searchMatrix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def searchMatrix(self, matrix:list[list[int]], target:int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+        l,r = 0, m*n-1
+        while l<=r:
+            mid = (l+r)//2
+            i = mid//n
+            j = mid%n
+            if matrix[i][j]<target: l=mid+1
+            elif matrix[i][j]>target: r=mid-1
+            else: return True
+        return False
