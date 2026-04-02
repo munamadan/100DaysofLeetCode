d4dd27806cfef06bf4702d787bde8394d850e21a
munamadan <dipankharel@gmail.com>
2026-04-02 10:13:55 +0545

    Binary Search Method

--- searchInsert.py
+++ searchInsert.py
@@ -1,3 +1,6 @@
+from msilib import make_id
+
+
 class Solution:
     def searchInsert(self, nums:list[int], target:int) -> int:
         n = len(nums)
@@ -9,3 +12,13 @@ class Solution:
                 return i
 
 #ez af
+class Solution:
+    def searchInsert(self, nums:list[int], target:int) -> int:
+        l,r = 0, len(nums)-1
+        while l<=right:
+            m = (l+r)//2
+            if nums[m]<target: l=mid+1
+            elif nums[m]>target: r=mid-1
+            else: return m
+
+        return l
